c4dd20e243de95bdd0437fc31b7061e7b1982b2f
Jack-Huynh <70099600+Jack-Huynh@users.noreply.github.com>
2020-08-23 20:59:10 +0700

    [feature/login] create new function

---
+++ Helper/databaseHelper.php
@@ -0,0 +1,17 @@
+<?php
+	function connectToDB() {
+		$hostname='localhost';
+		$username='root';
+		$password='';
+		$dbname='MyOnlineShop';
+		$conn=mysqli_connect($hostname, $username, $password, $dbname);
+		if(!$conn) {
+			die('Khong the ket noi'.mysql_error($conn));
+			exit();
+		}
+		return $conn;	
+	}
+	function disconnectToDB($conn) {
+		mysqli_close($conn);
+	}
+?>
