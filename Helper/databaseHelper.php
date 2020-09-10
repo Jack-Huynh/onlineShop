37fd1ea2e8f3a21abfa818faee780e55b74d3819
Jack-Huynh <70099600+Jack-Huynh@users.noreply.github.com>
2020-09-10 20:10:36 +0700

    [feature user]

--- Helper/databaseHelper.php
+++ Helper/databaseHelper.php
@@ -3,7 +3,7 @@
 		$hostname='localhost';
 		$username='root';
 		$password='';
-		$dbname='MyOnlineShop';
+		$dbname='myonlineshop';
 		$conn=mysqli_connect($hostname, $username, $password, $dbname);
 		if(!$conn) {
 			die('Khong the ket noi'.mysql_error($conn));
