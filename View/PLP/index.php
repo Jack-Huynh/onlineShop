762500e994996d72b3a888df0b4e0333df9afccf
Jack-Huynh <hoangphuchuynh2003@gmail.com>
2023-02-28 01:09:53 -0400

    little update for slide height

--- View/PLP/index.php
+++ View/PLP/index.php
@@ -38,39 +38,38 @@
 			$categoriesArray=loadCategoriesData(true, true);
 		?>
 		<div class="container col-sm-12">
-			<div class="row" style="border: solid; height:522px;">
+			<div class="row" style="border: solid;">
 				<div id="myCarousel" class="carousel slide" data-ride="carousel">
-  <!-- Indicators -->
-		  <ol class="carousel-indicators">
-		    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-		    <li data-target="#myCarousel" data-slide-to="1"></li>
-		    <li data-target="#myCarousel" data-slide-to="2"></li>
-		  </ol>
+				  <ol class="carousel-indicators">
+				    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+				    <li data-target="#myCarousel" data-slide-to="1"></li>
+				    <li data-target="#myCarousel" data-slide-to="2"></li>
+				  </ol>
 
 		  <!-- Wrapper for slides -->
-		  <div class="carousel-inner">
-		    <div class="item active">
-		      <img src="http://localhost/onlineShop/onlineShop/Image/products/slide1.jpg" alt="">
-		  	</div>
+				  <div class="carousel-inner">
+				    <div class="item active">
+				      <img src="http://localhost/onlineShop/onlineShop/Image/products/slide1.jpg" alt="">
+				  	</div>
 
-		    <div class="item">
-		      <img src="http://localhost/onlineShop/onlineShop/Image/products/slide2.jpg" alt="">
-		    </div>
+				    <div class="item">
+				      <img src="http://localhost/onlineShop/onlineShop/Image/products/slide2.jpg" alt="">
+				    </div>
 
-		    <div class="item">
-		      <img src="http://localhost/onlineShop/onlineShop/Image/products/slide3.jpg" alt="">
-		    </div>
-		  </div>
+				    <div class="item">
+				      <img src="http://localhost/onlineShop/onlineShop/Image/products/slide3.jpg" alt="">
+				    </div>
+				  </div>
 
-  <!-- Left and right controls -->
-		  <a class="left carousel-control" href="#myCarousel" data-slide="prev">
-		    <span class="glyphicon glyphicon-chevron-left"></span>
-		    <span class="sr-only">Previous</span>
-		  </a>
-		  <a class="right carousel-control" href="#myCarousel" data-slide="next">
-		    <span class="glyphicon glyphicon-chevron-right"></span>
-		    <span class="sr-only">Next</span>
-		  </a>
+		  <!-- Left and right controls -->
+				  <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+				    <span class="glyphicon glyphicon-chevron-left"></span>
+				    <span class="sr-only">Previous</span>
+				  </a>
+				  <a class="right carousel-control" href="#myCarousel" data-slide="next">
+				    <span class="glyphicon glyphicon-chevron-right"></span>
+				    <span class="sr-only">Next</span>
+				  </a>
 				</div>
 			</div>
 			
